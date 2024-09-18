5f87a6ace8f2c0aa123d83c2b2c2e075a086d6f2
LeoMedeirosss <lmf@cesar.school>
2024-09-18 15:31:01 -0300

    components 1.0

---
+++ hello-world/src/components/OtherComponent.jsx
@@ -0,0 +1,11 @@
+const OtherComponent = () => {
+
+    return(
+        <div>
+            <a href="https://www.google.com/webhp?hl=pt-BR&sa=X&ved=0ahUKEwj6jpPaj82IAxWnpZUCHadeFJoQPAgI" target="_blank">Link</a>
+        </div>
+    );
+
+};
+
+export default OtherComponent;
