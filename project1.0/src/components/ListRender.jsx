391487c51c09d37060cd5b099b4eeb3bdf29ed69
LeoMedeirosss <lmf@cesar.school>
2024-10-08 17:16:58 -0300

    advanced3.2 - key property

--- project1.0/src/components/ListRender.jsx
+++ project1.0/src/components/ListRender.jsx
@@ -6,10 +6,21 @@ const ListRender = () => {
 
     const [list] = useState(["nome1", "nome2", "nome3"]);
 
+    const [users] = useState([
+        {id:1, name:"João", age:31},
+        {id:2, name:"nome2", age:19},
+        {id:3983, name:"Pedro", age:49},
+    ])
+
     return (
     <div>
         <ul>
-            {list.map((item) => (<li>{item}</li>))}
+            {list.map((item, i) => (<li key={i}>{item}</li>))}
+        </ul>
+        <ul>
+            {users.map((users)=>(
+                <li key={users.id}> {users.name} - {users.age}</li>
+            ))}
         </ul>
     </div>
     )
