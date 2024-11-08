77f3e2f765dee503ffce509cf43261ce766a4e26
LeoMedeirosss <lmf@cesar.school>
2024-11-08 16:34:46 -0300

    forms 2.1 - label - input

--- formularios/src/Components/Forms.jsx
+++ formularios/src/Components/Forms.jsx
@@ -8,6 +8,10 @@ const Forms = () => {
             <label htmlFor="nome">Nome: </label>
             <input type="text" name='nome' placeholder='Digite seu nome' />
         </div>
+        <label>
+          <span>E-mail</span>
+          <input type="email" name='email' placeholder='Digite o seu e-mail'/>
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
