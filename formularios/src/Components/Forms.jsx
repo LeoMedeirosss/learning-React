c49ad468105f8d4fc80f4727e9c01441b571b75f
LeoMedeirosss <lmf@cesar.school>
2024-11-09 15:37:13 -0300

    forms 2.2 - gerenciamento de inputs

--- formularios/src/Components/Forms.jsx
+++ formularios/src/Components/Forms.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import './Forms.css'
 
 const Forms = () => {
+  const {nome, setNome} = useState()
+  const {email, setEmail} = useState()
+
+  const handleNome = (e) =>{
+    setNome(e.target.value)
+  };
+
   return (
     <div>
       <form>
         <div>
             <label htmlFor="nome">Nome: </label>
-            <input type="text" name='nome' placeholder='Digite seu nome' />
+            <input type="text" name='nome' placeholder='Digite seu nome' onChange={handleNome} />
         </div>
         <label>
           <span>E-mail</span>
