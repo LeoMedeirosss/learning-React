a10b3064c5c537972410de062c3966a56771f52f
LeoMedeirosss <lmf@cesar.school>
2024-12-11 19:30:30 -0300

    router 3.2

--- route/src/pages/Product.jsx
+++ route/src/pages/Product.jsx
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch";
 
 const Products = () => {
 
   const {id} = useParams();
 
-  return <div>
+  const url = "http://localhost:3000/products/" + id
+  const {data: product, loading, error} = useFetch(url)
+
+  return( <>
         <p>id do produto: {id}</p>
-    </div>
+        {error && <p>ocorreu um erro...</p>}
+        {loading && <p>Carregando</p>}
+        {product && (
+            <div>
+              <h1>{product.name}</h1>
+              <h3>R${product.price}</h3>
+            </div>
+        )}
+    </>
+  )
 }
 
 export default Products
